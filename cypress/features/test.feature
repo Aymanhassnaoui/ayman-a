023508274cacff87f3582bf1c387a18dd124a8bf
ayman hassnaoui <ayman12hassnaoui@gmail.com>
2024-12-16 15:51:08 +0100

    ajouter le fichier jenkins 2

--- cypress/features/test.feature
+++ cypress/features/test.feature
@@ -1,13 +1,26 @@
-Feature: test1
+Feature: Connexion à l'application
 
-  @regression
-  Scenario: scene1
-  
-    Given je suis ici
-    Then je suis content
+  @loginSuccess
+  Scenario Outline: Connexion avec un nom d'utilisateur valide et un mot de passe valide
+    Given utilisateur est sur la page de connexion
+    When utilisateur entre un nom utilisateur valide "username"
+    And utilisateur entre un mot de passe valide "secret_sauce"
+    And utilisateur clique sur le bouton de connexion
+    Then l’utilisateur est connecté avec succès
 
-  @smoke
-  Scenario: scene2
-  
-    Given je suis ici2
-    Then je suis content3
+
+
+  @loginFailure
+  Scenario: Connexion avec un nom d'utilisateur valide et un mot de passe non valide
+    Given utilisateur est sur la page de connexion
+    When utilisateur entre un nom utilisateur valide "standard_user"
+    And utilisateur entre un mot de passe non valide  "Admin"
+    And utilisateur clique sur le bouton de connexion
+    Then utilisateur avoir un message erreur
+
+  Scenario: Connexion avec un nom d'utilisateur  nom valide et un mot de passe  valide
+    Given utilisateur est sur la page de connexion
+    When utilisateur entre un nom utilisateur non valide "12Admin"
+    And utilisateur entre un mot de passe valide "secret_sauce"
+    And utilisateur clique sur le bouton de connexion
+    Then utilisateur avoir un message erreur
--- cypress/steps/test1.steps.js
+++ cypress/steps/test1.steps.js
@@ -2,26 +2,47 @@
 /// <reference types="cypress" />
 
 import { Given, When } from "cypress-cucumber-preprocessor/steps";
-Given("je suis ici",  () => {
-    assert.equal("a", "a");
-})
 
-Then('je suis content', () => {
+
+Given('utilisateur est sur la page de connexion', () => {
   // Write code here that turns the phrase above into concrete actions
-  assert.equal("a", "b");
-})
+}) 
 
-
-
-Given('je suis ici2', () => {
+When('utilisateur entre un nom utilisateur valide {string}', (s) => {
   // Write code here that turns the phrase above into concrete actions
-   assert.equal("a", "a");
 })
 
-Then('je suis content3', () => {
+When('utilisateur entre un mot de passe valide {string}', (s) => {
   // Write code here that turns the phrase above into concrete actions
-   assert.equal("a", "a");
 })
+
+When('utilisateur clique sur le bouton de connexion', () => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+Then('l’utilisateur est connecté avec succès', () => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+Given('utilisateur est sur la page de connexion', () => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+Then('utilisateur avoir un message erreur', () => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+When('utilisateur entre un mot de passe non valide  {string}', (s) => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+When('utilisateur entre un nom utilisateur non valide {string}', (s) => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+
+
+
 
 
 
